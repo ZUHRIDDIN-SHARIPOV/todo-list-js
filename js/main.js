c054cb992e1eb66af0de770cbfc8b03f8bbc121e
ZUHRIDDIN-SHARIPOV <zuhriddinshx@gmail.com>
2024-01-24 23:16:03 +0500

    The hero part of the site has been added.

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const select = document.querySelector("select");
+const options = document.querySelectorAll("option");
+const addButton = document.querySelector(".site-nav__form-btn");
+const editButton = document.querySelector(".site-nav__form-edit-btn");
+const searchButton = document.querySelector(".site-nav__form-search-btn");
+
+const buttons = {
+  add: addButton,
+  edit: editButton,
+  search: searchButton,
+  click: function () {
+    this.add.onclick = () => {
+      this.add.style.transform = "scale(0.9)";
+      setTimeout(() => {
+        this.add.style.transform = "scale(1)";
+      }, 55);
+    };
+    this.edit.onclick = () => {
+      this.edit.style.transform = "scale(0.9)";
+      setTimeout(() => {
+        this.edit.style.transform = "scale(1)";
+      }, 55);
+    };
+    this.search.onclick = () => {
+      this.search.style.transform = "scale(0.9)";
+      setTimeout(() => {
+        this.search.style.transform = "scale(1)";
+      }, 55);
+    };
+  },
+};
+buttons.click();
+
+input.oninput = () => {
+  const inputValue = input.value;
+  const selectedValue = select.value;
+  const defaultValue = options[0].value;
+  const searchValue = options[1].value;
+
+  if (selectedValue === searchValue) {
+    addButton.classList.add("hidden");
+    searchButton.classList.add("show");
+    console.log(inputValue);
+  } else {
+    searchButton.classList.remove("show");
+    addButton.classList.remove("hidden");
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = input.value;
+  const selectedValue = select.value;
+  const defaultValue = options[0].value;
+  const searchValue = options[1].value;
+
+  if (selectedValue === defaultValue) {
+    console.log(inputValue);
+  }
+});
